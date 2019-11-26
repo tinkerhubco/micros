a14c1fe797166953a7277cc078d083394ce7722b
isotopeee <johnmichael.doroy@gmail.com>
2019-11-26 22:55:11 +0800

    feat(mailer): implement email sending (#7)
    * refactor: rename `hello` to `ping`
    
    * improvement: make routes private
    
    * feat: set up an API key for mailer microservice
    
    * improvement: use `nodejs12.x` runtime
    
    Closes #5

--- packages/mailer/handler.ts
+++ packages/mailer/handler.ts
@@ -1,12 +1,50 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
 import 'source-map-support/register';
+import * as sgMail from '@sendgrid/mail';
 
-export const hello: APIGatewayProxyHandler = async (event, _context) => {
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+export const ping: APIGatewayProxyHandler = async (event, _context) => {
   return {
     statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
-      input: event,
-    }, null, 2),
+    body: JSON.stringify(
+      {
+        message:
+          'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
+        input: event
+      },
+      null,
+      2
+    )
   };
-}
+};
+
+export const send: APIGatewayProxyHandler = async (event, _context) => {
+  const { body: requestBody } = event;
+
+  const {
+    dynamicTemplateData,
+    from = {
+      name: 'No Reply',
+      email: 'noreply@micros.tinkerhub.dev'
+    },
+    subject,
+    templateId,
+    to
+  } = JSON.parse(requestBody);
+
+  const [
+    { body: responseBody, statusCode: responseStatusCode }
+  ] = await sgMail.send({
+    dynamicTemplateData,
+    from,
+    subject,
+    templateId,
+    to
+  });
+
+  return {
+    body: responseBody,
+    statusCode: responseStatusCode
+  };
+};
